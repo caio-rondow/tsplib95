b63c510ec7dcb20103793b7b7428b41a9d1d548e
Olavo-B <olavo.barros@ufv.br>
2023-05-16 14:45:59 -0300

    modified:   annealing.cpp 	new file:   result/Resultados.png 	new file:   result/result_SA_burma14.txt 	modified:   run_tspp.sh

--- annealing.cpp
+++ annealing.cpp
@@ -7,6 +7,7 @@
 #include <numeric>
 #include <chrono>
 #include <cmath>
+#include <random>
 
 #define EULER 2.7182818
 
@@ -58,19 +59,37 @@ vector<vector<int>> load_problem(const std::string &filename)
     return matrix;
 }
 
-int evaluate(vector<int> route, const vector<vector<int>> &distance, const vector<vector<int>> &penalty, int n)
-{
+/* recalculate all path cost */
+int total_path_cost(vector<int>route, const vector<vector<int>>&distance, const vector<vector<int>>&penalty, int n){
     int cost = 0;
     int src, tar;
 
-    for (int i = 0; i < n; i++)
-    {
+    for(int i=0; i<n; i++){
         src = route[i];
-        tar = route[(i + 1) % n];
+        tar = route[(i+1)%n];
         cost += (distance[src][tar] + penalty[src][i]);
     }
 
     return cost;
+}
+
+int evaluate(int initial_cost, int beg, int end, const vector<int>&route, const vector<vector<int>>&distance, const vector<vector<int>>&penalty, int n){
+
+	/* calculate new distance cost */
+	int dist_cost = -distance[route[beg]][route[(beg+1)%n]]
+					-distance[route[end]][route[(end+1)%n]]
+					+distance[route[beg]][route[end]]
+					+distance[route[(beg+1)%n]][route[(end+1)%n]];
+
+	/* calculate new penalty cost */
+	int k = (beg+1)%n;
+	int pen_cost=0;
+	for(int i=end; i>beg; i--){
+		pen_cost += (penalty[route[i]][k] - penalty[route[i]][i]);
+		k++;
+	} 
+
+	return initial_cost + dist_cost + pen_cost;
 }
 
 float temperature(int temp)
@@ -80,8 +99,8 @@ float temperature(int temp)
 
 float acc_probability(int deltaC, double temp)
 {
-    // return ( deltaC<0 ? 1 : exp((-1*deltaC)/temp) );
-    return (1 / ( 1 + exp(deltaC / 1000.0)) );
+    return ( deltaC<0 ? 1 : exp((-1.0*deltaC)/temp) );
+    // return (1.0 / ( 1.0 + exp(deltaC / 1000.0)) );
 }
 
 int annealing(const vector<vector<int>> &distance, const vector<vector<int>> &penalty, int n)
@@ -90,54 +109,94 @@ int annealing(const vector<vector<int>> &distance, const vector<vector<int>> &pe
     /* RANDOM INITIAL SOLUTION */
     vector<int> route(n);
     iota(route.begin(), route.end(), 0);
-    // random_shuffle(route.begin()+1, route.end());
 
     /* INITIAL COST */
-    int curr_cost = evaluate(route, distance, penalty, n);
-    double temp = 1000.0;
+    int curr_cost = total_path_cost(route, distance, penalty, n);
+    double temp = n >= 666 ? 100.0:1000.0;
     int nswaps = 0;
-    int besti = -1, bestj = -1;
 
-    cout << "INITIAL COST: " << curr_cost << "\n";
+    /* RANDOM VALUE*/
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<float> dis(0.0, 1.0);
 
-    while (temp >= 0.0001)
-    {
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
+    int reverse_beg = 0;
+    int reverse_end = 0;
 
-                /* EVALUATE NEW COST */
-                swap(route[i], route[j]);
+    while (temp >= 0.0001){
 
-                int new_cost = evaluate(route, distance, penalty, n);
-                float prob = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+        /* search in the neighborhood space */
+        for(int i=0; i<n-2; i++){
+            for(int j=i+2; j<n; j++){
 
-                /* EFFECT SWAP */
-                if (new_cost < curr_cost || prob < acc_probability(curr_cost - new_cost, temp))
-                {
-                    curr_cost = new_cost;
-                    besti = i;
-                    bestj = j;
+                /* calculate 2opt new cost */
+				int solution_cost = evaluate(curr_cost,i,j,route,distance,penalty,n);
+
+                float randomNum = dis(gen);
+
+                /* found better solution */
+                if(solution_cost < curr_cost || randomNum < acc_probability((solution_cost - curr_cost),temp)){
+                    reverse_beg = (i+1)%n;
+                    reverse_end = j;
+                    curr_cost = solution_cost;
+                    reverse(route.begin()+reverse_beg, route.begin()+reverse_end+1);
                     nswaps++;
                 }
-
-                /* UNDO SWAP */
-                swap(route[i], route[j]);
             }
         }
 
-        swap(route[besti], route[bestj]);
-
-        // for (auto &r : route)
-        //     cout << r << " ";
-        // cout << "\n";
-
-        /* UPDATE TEMPERATURE */
         temp = temperature(temp);
     }
 
-    cout << "NUM. SWAPS: " << nswaps << "\n";
+    return curr_cost;
+}
+
+int tannealing(const vector<vector<int>> &distance, const vector<vector<int>> &penalty, int n)
+{
+
+    /* RANDOM INITIAL SOLUTION */
+    vector<int> route(n);
+    iota(route.begin(), route.end(), 0);
+
+    /* INITIAL COST */
+    int curr_cost = total_path_cost(route, distance, penalty, n);
+    double temp = n >= 666 ? 100.0:1000.0;
+    int nswaps = 0;
+
+    /* RANDOM VALUE*/
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<float> dis(0.0, 1.0);
+
+    int reverse_beg = 0;
+    int reverse_end = 0;
+
+    while (temp >= 0.0001){
+
+        /* search in the neighborhood space */
+        for(int i=0; i<n-2; i++){
+            for(int j=i+2; j<n; j++){
+
+                /* calculate 2opt new cost */
+				int solution_cost = evaluate(curr_cost,i,j,route,distance,penalty,n);
+
+                float randomNum = dis(gen);
+
+                /* found better solution */
+                if(solution_cost < curr_cost || randomNum < acc_probability((solution_cost - curr_cost),temp) ){
+                    reverse_beg = (i+1)%n;
+                    reverse_end = j;
+                    curr_cost = solution_cost;
+                    reverse(route.begin()+reverse_beg, route.begin()+reverse_end+1);
+                    nswaps++;
+                }else{
+
+                }
+            }
+        }
+
+        temp = temperature(temp);
+    }
 
     return curr_cost;
 }
@@ -155,11 +214,18 @@ int main(int argc, char **argv)
     vector<vector<int>> distance = load_problem(problem_name);
     vector<vector<int>> penalty = load_problem(penalty_name);
 
-    int ans=-1;
-    for(int i=0; i<25; i++){
-        ans = max( ans, annealing(distance, penalty, distance.size()) );
-    }
-    cout << "FINAL COST: " << ans << "\n";
+    // int times = 100;
+    // while(times--){
+    //     int ans = annealing(distance,penalty,distance.size());
+    //     if(ans == 30){
+    //         cout << "BEST! " << ans << "\n";
+    //         break;
+    //     } else{
+    //         cout << "ans.: " << ans << "\n";
+    //     } 
+    // }
+
+    cout << annealing(distance, penalty, distance.size());
 
     return 0;
 }
